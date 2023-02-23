0f0c5e335c0485c8459a7ba51375a0b57ee6a2f1
IFNEF <69453132+IFNEF@users.noreply.github.com>
2023-02-23 18:22:25 +0800

    计算输入数的阶乘

--- main.c
+++ main.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    double a;
-    scanf("%lf",&a);
-    double z=a-23.0;
-    if(z>=0){
-        printf("找零：%.2f元\n",z);
-    }
-    else
+    int m=0,n=0,i=1;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
     {
-        printf("没钱滚。");
+        m=i*m;
     }
+    printf("%d",m);
     return 0;
 }
