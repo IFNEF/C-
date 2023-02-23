78d453bc48c9818af3968f5015904709cdafd3d3
IFNEF <69453132+IFNEF@users.noreply.github.com>
2023-02-23 17:04:21 +0800

    Update main.c

--- main.c
+++ main.c
@@ -3,16 +3,17 @@
 
 int main()
 {
-    int a=rand()%100+1,b=0;
+    int a=rand()%100+1,b=0,t=0;
     do{
 
     scanf("%d",&b);
+    t++;
         if(b>a)
         printf("大了\n");
         else if(b<a)
             printf("小了\n");
     }
     while(b!=a);
-    printf("您猜对了");
+    printf("您猜对了,使用%d次就猜出了随机数结果。",t);
     return 0;
 }
