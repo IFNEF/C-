84bae849dbbbf83fac0abbdd173c077dee57e4e0
IFNEF <69453132+IFNEF@users.noreply.github.com>
2023-02-23 17:29:14 +0800

    计算任意数均值，输入-1结束

--- main.c
+++ main.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    double a;
-    scanf("%lf",&a);
-    double z=a-23.0;
-    if(z>=0){
-        printf("æ‰¾é›¶ï¼š%.2få…ƒ\n",z);
-    }
-    else
+    int s=0,x=0,n=0;
+    double a=0;
+    do{
+    scanf("%d",&x);
+    if(x!=-1)
     {
-        printf("æ²¡é’±æ»šã€‚");
+      s+=x;
+      n++;
+      a=s/n;
     }
+    }
+    while(x!=-1);
+    printf("%d¸öÊýµÄ¾ùÖµÎª%.2lf¡£",n,a);
     return 0;
 }
